0773fcb16b95970ee20805ffe654021de4abe21d
Zachary Perales <zacharyromeperales@gmail.com>
2022-05-06 04:28:27 -0400

    Added scss stylesheet.

--- archived-service/views/resources/bootstrap/js/bootstrap.bundle.js
+++ archived-service/views/resources/bootstrap/js/bootstrap.bundle.js
@@ -4824,7 +4824,7 @@
 
         if (this._isBodyOverflowing) {
           // Note: DOMNode.style.paddingRight returns the actual value or '' if not set
-          //   while $(DOMNode).css('padding-right') returns the calculated value or 0 if not set
+          //   while $(DOMNode).scss('padding-right') returns the calculated value or 0 if not set
           // Adjust fixed content padding
           $$$1(Selector.FIXED_CONTENT).each(function (index, element) {
             var actualPadding = $$$1(element)[0].style.paddingRight;
